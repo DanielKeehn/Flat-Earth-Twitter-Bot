3ab4a80db899bdd442f0f80ed09e67f89c7c454e
DanielKeehn <danielkeehn@bellsouth.net>
2019-06-10 10:01:59 +0200

    Config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Put your own Twitter App keys here. See README.md for more detail.
 module.exports = {
-  consumer_key:         'E9E1rjMWWINnCKtDkgwTPuuEr',
-  consumer_secret:      'IYLdO0CxoOUgLwLUbQEyfeQDxwpRmleJzj3o1WgUr7wdjks4Ay',
-  access_token:         '1055887978655072257-CYOpxXs5OEkquMCxwIRcI5agR1CYg5',
-  access_token_secret:  '5VTCcaflqK9f6AkpqXFMDxPGqDEYRT3vwCW0ezsdFvzs6'
+  consumer_key:         'INSERT_HERE',
+  consumer_secret:      'INSERT_HERE',
+  access_token:         'INSERT_HERE',
+  access_token_secret:  'INSERT_HERE'
 }
